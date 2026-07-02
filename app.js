8c199b454062a64c9fb76258677f12ff12882b1a
peterhsumuen <peter@symptomtrace.com>
2026-07-02 16:23:11 -0700

    booking system for google accs 3

--- app.js
+++ app.js
@@ -392,8 +392,7 @@ renderPortfolioCategories();
 renderFAQ();
 listenBookingSlots();
 
-const BOOKING_API_URL = "https://script.google.com/macros/u/0/s/AKfycbwr53AxuED3AVtyoN4-rGZb_fPeKW-QP-eKpKd8MKCH1tJnfEoQfMYgmgb6Jr2A0fHU/exec";
-const BOOKING_AUTHUSER = "0";
+const BOOKING_API_URL = "https://script.google.com/macros/s/AKfycbwr53AxuED3AVtyoN4-rGZb_fPeKW-QP-eKpKd8MKCH1tJnfEoQfMYgmgb6Jr2A0fHU/exec";
 
 const bookingSettings = {
   serviceDurationMinutes: 180,
@@ -807,7 +806,6 @@ function callBookingApi(action, params) {
   return new Promise((resolve, reject) => {
     const callbackName = `bookingCallback_${Date.now()}_${Math.random().toString(36).slice(2)}`;
     const url = new URL(BOOKING_API_URL);
-    url.searchParams.set("authuser", BOOKING_AUTHUSER);
     url.searchParams.set("action", action);
     url.searchParams.set("callback", callbackName);
     Object.entries(params).forEach(([key, value]) => url.searchParams.set(key, value || ""));
